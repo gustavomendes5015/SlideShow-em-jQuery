68b0694ba4fd5dd49cc23c4ee617b06b798ef864
zbt5015 <gugutb@hotmail.com>
2014-07-19 23:06:50 -0300

    SlideShow

--- js/slide.js
+++ js/slide.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+﻿$(document).ready(function() {
     //Quando documento estiver pronto
     efeito();//chamar função responsável pelos efeitos
     $(".slide").hover(function() {
